8c1af8b94dc96bc8266fed9e47611f7829fdc71f
benzsupaluk <supaluk.j@ku.th>
2021-01-16 16:16:22 +0700

    feat: add changing image function to ref card

--- src/components/homepage/MakeupByRefCard.vue
+++ src/components/homepage/MakeupByRefCard.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="makeup-ref-card">
-    <img class="cosmetic-bg" src="../../assets/images/eyeshadow-pink.png" />
+    <img class="cosmetic-bg" src="@/assets/images/eyeshadow-pink.png" />
     <div class="ref-image-container">
       <div class="ref-img-card">
-        <img class="ref-img" src="../../assets/images/lisa.jpg" />
+        <img id="ref-image" class="ref-img fadein" :src="require(`@/assets/images/${imgURL}`)" />
         <div class="ref-body">
           <div class="ref-detail">
             "Neques porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
             velit..."
           </div>
           <div class="mt-5">
-            <a class="makeup-ref-btn" href=""
-              >Makeup by Reference.<i class="fas fa-images pl-1"></i
-            ></a>
+            <a class="makeup-ref-btn" href="">Makeup by Reference. </a>
           </div>
         </div>
       </div>
@@ -21,17 +19,98 @@
   </div>
 </template>
 
+<script>
+import $ from 'jquery';
+
+export default {
+  data() {
+    return {
+      imgURL: 'lisa.jpg',
+    };
+  },
+  created() {
+    var images = ['lisa-1.png', 'lisa-2.png', 'lisa.jpg'];
+    var index = 0;
+
+    setInterval(() => {
+      $('#ref-image').fadeOut('slow', () => {
+        this.imgURL = images[index];
+        index++;
+        if (index === images.length) {
+          index = 0;
+        }
+        $('#ref-image').fadeIn('slow');
+      });
+    }, 5000);
+  },
+};
+</script>
+
 <style lang="scss" scoped>
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@-moz-keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@-ms-keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@-o-keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
 .makeup-ref-btn {
   font-size: 1.5rem;
   text-decoration: underline;
   color: #222;
 }
+
 .makeup-ref-bg {
   display: flex;
 }
+
 .makeup-ref-card {
   padding: 0 3rem;
+}
+
+.fadeIn {
+  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 2s; /* Firefox < 16 */
+  -ms-animation: fadein 2s; /* Internet Explorer */
+  -o-animation: fadein 2s; /* Opera < 12.1 */
+  animation: fadein 2s;
 }
 
 .cosmetic-bg {
