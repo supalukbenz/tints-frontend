eb9ac910dac1b45e8c3ff508e24fe4b77456f350
benzsupaluk <supaluk.j@ku.th>
2021-04-27 19:13:53 +0700

    feat: change api path

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = 'https://tint-api-3774d.ondigitalocean.app/api/';
+axios.defaults.baseURL = 'https://tint-api-2-h22gy.ondigitalocean.app/api/';
 // axios.defaults.baseURL = 'http://127.0.0.1:5000/api/';
 
 new Vue({
