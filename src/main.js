4cd6cbbf30d8dd5296fbfd638ac5da68c66aca07
benzsupaluk <supaluk.j@ku.th>
2021-04-29 22:24:45 +0700

    feat: merge with feature/recommendation

--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@ import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false;
 // axios.defaults.baseURL = 'https://tint-api-2-h22gy.ondigitalocean.app/api/';
-axios.defaults.baseURL = 'https://tint-api-3774d.ondigitalocean.app/api/';
+// axios.defaults.baseURL = 'https://tint-api-3774d.ondigitalocean.app/api/';
 
-// axios.defaults.baseURL = 'http://127.0.0.1:5000/api/';
+axios.defaults.baseURL = 'http://127.0.0.1:5000/api/';
 
 new Vue({
   router,
